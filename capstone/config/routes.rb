fb6cc89b17d014fb1f2e82a6d2916a03ad94d526
Ted <ekeast328@gmail.com>
2016-04-23 14:25:25 -0400

    Facebook Authentication

--- capstone/config/routes.rb
+++ capstone/config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  get 'welcome/index'
+
+  #get 'sessions/new'
+
+  #get 'sessions/create'
+
+  #get 'sessions/failure'
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+  resource :welcome, only: [:index]
+
+  root to: "welcome#index"  # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
